7b83ab40d3deb1ef0541a7887d333eb0edbb2aa6
AhmedEmadElDeen14 <ahmedemadeldeen0@gmail.com>
2024-07-10 02:19:17 +0300

    Finished App Bar Ui

--- lib/features/products/presentation/pages/product_list.dart
+++ lib/features/products/presentation/pages/product_list.dart
@@ -1,12 +1,71 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:route_task_flutter/core/utils/app_colors.dart';
 
-class ProductListScreen  extends StatelessWidget {
-  const ProductListScreen ({super.key});
+class ProductListScreen extends StatelessWidget {
+  const ProductListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: AppColors.whiteColor,
+      appBar: AppBar(
+        toolbarHeight: 100,
+        backgroundColor: Colors.transparent,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              image: AssetImage("assets/images/logo.png"),
+              height: 22.h,
+              width: 66.w,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 46.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      border: Border.all(color: AppColors.blueColor),
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(borderSide: BorderSide.none),
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: AppColors.blueColor,
+                          size: 30,
+                        ),
+                        hintText: 'what do you search for?',
+                        hintStyle: GoogleFonts.poppins(
+                          color: AppColors.fontColor.withOpacity(.6),
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  child: Icon(
+                    Icons.shopping_cart_outlined,
+                    color: AppColors.blueColor,
+                    size: 30,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
