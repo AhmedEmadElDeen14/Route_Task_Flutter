fbc8395a29795f57b44da0c5d687c841aa3f2097
AhmedEmadElDeen14 <ahmedemadeldeen0@gmail.com>
2024-07-11 15:11:51 +0300

    Update Styles file with text style

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:route_task_flutter/core/utils/app_colors.dart';
+
+
+class BodyTextStyle {
+
+  static TextStyle fontNormal = GoogleFonts.poppins(
+  fontSize: 14,
+  color: AppColors.fontColor,
+  ) as TextStyle;
+}
